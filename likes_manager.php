094a4ed8ad9c1582578a735fe85046518467c664
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-12-01 19:53:39 +0200

    Updated .gitignore to ignore user uploads Updated likes_manager to preserve correct likes amount incase of 2 queries happening at once.

--- likes_manager.php
+++ likes_manager.php
@@ -9,64 +9,75 @@ if (isset($_POST['likes']) && isset($_POST['image_path']) && isset($_POST['image
 	$image_path = $_POST['image_path'];
 	$user_id = $_SESSION['user_id'];
 
-	$query = "SELECT * FROM `images` WHERE `path` = ? && `id` = ?";
-	$stmt = $conn->prepare($query);
-	$stmt->execute([$image_path, $image_id]);
-	$res = $stmt->fetch(PDO::FETCH_ASSOC);
-	unset($stmt);
-	if ($res)
+	try
 	{
-		$likes = $res['likes'];
-		$likes += 1;
-		if ($image_id == $res['id'])
+		$conn->beginTransaction();
+		$query = "SELECT * FROM `images` WHERE `path` = ? && `id` = ?";
+		$stmt = $conn->prepare($query);
+		$stmt->execute([$image_path, $image_id]);
+		$res = $stmt->fetch(PDO::FETCH_ASSOC);
+		unset($stmt);
+		if ($res)
 		{
-			$image_owner_id = $res['user_id'];
-			if ($image_owner_id == $user_id)
-				$liker = "You";
-			else
-				$liker = $_SESSION['username'];
-			
-			$query = "SELECT `liked` FROM `likes` WHERE `image_id` = ? 
-						&& `user_id` = ?";
-			$stmt = $conn->prepare($query);
-			$stmt->execute([$image_id, $user_id]);
-			$res = $stmt->fetch(PDO::FETCH_ASSOC);
-			unset($stmt);
-			if (!$res)
+			$likes = $res['likes'];
+			$likes += 1;
+			if ($image_id == $res['id'])
 			{
-				$query = "UPDATE `images` SET `likes` = ? 
-							WHERE `id` = ?";
-				$stmt = $conn->prepare($query);
-				$stmt->execute([$likes, $image_id]);
-				unset($stmt);
+				$image_owner_id = $res['user_id'];
+				if ($image_owner_id == $user_id)
+					$liker = "You";
+				else
+					$liker = $_SESSION['username'];
 				
-				$query = "INSERT INTO `likes` (`user_id`, `image_id`, `liked`)
-							VALUE (?, ?, ?)";
+				$query = "SELECT `liked` FROM `likes` WHERE `image_id` = ? 
+							&& `user_id` = ?";
 				$stmt = $conn->prepare($query);
-				$stmt->execute([$user_id, $image_id, 1]);
-				unset($stmt);
-				
-				$query = "SELECT * FROM `users` WHERE `id` = ?";
-				$stmt = $conn->prepare($query);
-				$stmt->execute([$image_owner_id]);
+				$stmt->execute([$image_id, $user_id]);
 				$res = $stmt->fetch(PDO::FETCH_ASSOC);
-
-				if ($res)
+				unset($stmt);
+				if (!$res)
 				{
-					if ($res['notifications'] == 1)
-						mail_like_notif($res['email'], $res['username'],$liker);
-				}
+					$query = "UPDATE `images` SET `likes` = ? 
+								WHERE `id` = ?";
+					$stmt = $conn->prepare($query);
+					$stmt->execute([$likes, $image_id]);
+					unset($stmt);
+					
+					$query = "INSERT INTO `likes` (`user_id`, `image_id`, `liked`)
+								VALUE (?, ?, ?)";
+					$stmt = $conn->prepare($query);
+					$stmt->execute([$user_id, $image_id, 1]);
+					unset($stmt);
+					
+					$query = "SELECT * FROM `users` WHERE `id` = ?";
+					$stmt = $conn->prepare($query);
+					$stmt->execute([$image_owner_id]);
+					$res = $stmt->fetch(PDO::FETCH_ASSOC);
 
-				echo $likes;
+					if ($res)
+					{
+						if ($res['notifications'] == 1)
+							mail_like_notif($res['email'], $res['username'],$liker);
+					}
+
+					echo $likes;
+				}
+				else
+					echo get_image_likes($image_path);
 			}
 			else
 				echo get_image_likes($image_path);
 		}
 		else
 			echo get_image_likes($image_path);
+
+		$conn->commit();
 	}
-	else
-		echo get_image_likes($image_path);
+	catch (Throwable $e)
+	{
+		$conn->rollback();
+		echo $e->getMessage();
+	}
 }
 else
 {
