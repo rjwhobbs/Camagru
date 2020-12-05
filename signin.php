b0fd28e6b61357e9b32d74352ccef9309b6ce89c
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-12-05 20:21:07 +0200

    Deleted commented code

--- signin.php
+++ signin.php
@@ -4,15 +4,6 @@ require (getcwd().'/app/controller.php');
 require (getcwd().'/form_block.php');
 require (getcwd().'/header.php');
 ?>
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Sign in</title>
-</head>
-<body> -->
 	<h1>Sign in</h1>
 	<div>
 		<?php 	
