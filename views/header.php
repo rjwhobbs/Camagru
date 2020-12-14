1ce4fea1ed4276a4f126d65ee4a962429b8bad58
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-12-14 15:31:03 +0200

    Basic header styling

--- views/header.php
+++ views/header.php
@@ -12,42 +12,47 @@ require ('./globals.php');
 	<link rel="stylesheet" href=<?php echo $root_ws."/css/style.css"?>>
 </head>
 <body>
-	<h1>This is Camagru</h1>
-	<?php
-		$redirect = "../index.php";
-		if ($_SERVER['REQUEST_URI'] == '/camagru/index.php')
-		{
-			$redirect = 'index.php';
-		}
-		if (isset($_SESSION['user_id'])) 
-		{?>
-			<a href=<?php echo $root_ws."/app/signout.php"?>><input type="submit" value="Sign Out"></a>
-			<a href=<?php echo $root_ws."/app/profile.php"?>><input type="submit" value="Profile"></a> 
-			<a href=<?php echo $root_ws."/app/editor.php"?>><input type="submit" value="Camera Editor"></a> 
-		<?php 
-		} 
-		else 
-		{?>
-			<a href=<?php echo $root_ws."/app/signin.php"?>><input type="submit" value="Sign In"></a>
-			<a href=<?php echo $root_ws."/app/form.php"?>><input type="submit" value="Sign Up"></a>
-		<?php 
-		}
-	?>
-	<a href=<?php echo $root_ws."/index.php"?>><input type="submit" value="Feed"></a>
-	<?php
-		if (isset($_SESSION['user_id']))
-		{?>
-			<br><span>Logged in as: <?php echo $_SESSION['username']?></span>
-			<?php
-				$profile_pic = get_profile_pic($_SESSION['user_id']);
-				if ($profile_pic !== FALSE)
-				{?>
-					<div class="profile-pic-div">
-						<img src=<?php echo $root_ws.'/'.$profile_pic ?> alt="">
-					</div>
-				<?php
-				}
+	<div class="main-header">
+	<div class="flex-con">
+		<h1 class="flex-item-1 mt-15 font-60">Camagru</h1>
+		<?php
+			if (isset($_SESSION['user_id']))
+			{?>
+				<div class="flex-item-01">			
+					<?php
+						$profile_pic = get_profile_pic($_SESSION['user_id']);
+						if ($profile_pic !== FALSE)
+						{?>
+							<div class="profile-pic-div">
+								<img src=<?php echo $root_ws.'/'.$profile_pic ?> alt="">
+							</div>
+						<?php
+						}
+					?>
+					<!-- <span>Logged in as: <?php echo $_SESSION['username']?></span> -->
+				</div>
+			<?php	
+			}
 			?>
-		<?php	
-		}
-	?>	
+	</div>
+	<div class="flex-con-button">
+		<a href=<?php echo $root_ws."/index.php"?>><input type="submit" value="Feed"></a>
+		<?php
+			if (isset($_SESSION['user_id'])) 
+			{?>
+				<a href=<?php echo $root_ws."/app/editor.php"?>><input type="submit" value="Camera Editor"></a> 
+				<a href=<?php echo $root_ws."/app/profile.php"?>><input type="submit" value="Profile"></a> 
+				<a href=<?php echo $root_ws."/app/signout.php"?>><input type="submit" value="Sign Out"></a>
+				<span class="pd-7 pdb-0 pdt-7 dark-border">Logged in as: <?php echo $_SESSION['username']?></span>
+			<?php 
+			} 
+			else 
+			{?>
+				<a href=<?php echo $root_ws."/app/signin.php"?>><input type="submit" value="Sign In"></a>
+				<a href=<?php echo $root_ws."/app/form.php"?>><input type="submit" value="Sign Up"></a>
+			<?php 
+			}
+		?>
+		
+	</div>
+	</div>	
