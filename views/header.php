d7578e89949edeb139ac1bd54b56fad72f90e6fb
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-12-14 11:23:12 +0200

    Doing the css things

--- views/header.php
+++ views/header.php
@@ -8,6 +8,7 @@ require ('./globals.php');
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Camagru</title>
+	<link rel="stylesheet" href=<?php echo $root_ws."/css/layout.css"?>>
 	<link rel="stylesheet" href=<?php echo $root_ws."/css/style.css"?>>
 </head>
 <body>
