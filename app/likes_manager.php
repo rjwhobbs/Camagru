fb1bb8ff23f1338bb4f0c4755d802ce2528cacc0
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2021-01-02 12:29:43 +0200

    do you even left join

--- app/likes_manager.php
+++ app/likes_manager.php
@@ -15,9 +15,9 @@ if (isset($_POST['likes']) && isset($_POST['image_path']) && isset($_POST['image
 		// Lets try use this join some where
 		// SELECT * FROM `images` INNER JOIN `likes` ON `images`.`id` = `likes`.`image_id` WHERE (`images`.`user_id` = 2 AND `likes`.`user_id` = 2) 
 		// or
-		$testq = "SELECT `images`.`likes`, `likes`.`liked` FROM `images` INNER JOIN `likes` ON `images`.`id` = `likes`.`image_id` WHERE (`images`.`id` = ? AND `likes`.`user_id` = ? AND `images`.`path` = ?)";
+		$testq = "SELECT `images`.`likes`, `likes`.`user_id`, `likes`.`liked` FROM `images` LEFT JOIN `likes` ON `images`.`id` = `likes`.`image_id` WHERE (`images`.`id` = ? AND `images`.`path` = ?)";
 		$tsmt = $conn->prepare($testq);
-		$tsmt->execute([$image_id, $user_id, $image_path]);
+		$tsmt->execute([$image_id, $image_path]);
 		$tres = $tsmt->fetch(PDO::FETCH_ASSOC);
 		var_dump($tres);
 
